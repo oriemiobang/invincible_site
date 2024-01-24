f111684c927a35ed9299c5ca1b55735d0466533c
oriemiobang <oriemiobango@gmail.com>
2024-01-24 21:12:16 +0300

    skills page

--- script.js
+++ script.js
@@ -36,3 +36,61 @@ function togglePages(direction) {
 
 }
 }
+
+// circle prosgress bar
+let number1 = document.getElementById('percentage1');
+let counter1 = 0;
+
+setInterval(() => {
+  if (counter1 == 95){
+    clearInterval;
+  }
+  else {
+    counter1 +=1;
+    number1.innerHTML = `${counter1}%`;
+  }
+}, 30);
+
+
+let number2 = document.getElementById('percentage2');
+let counter2 = 0;
+
+setInterval(() => {
+  if (counter2 == 90){
+    clearInterval;
+  }
+  else {
+    counter2 +=1;
+    number2.innerHTML = `${counter2}%`;
+  }
+}, 30);
+
+
+let number3 = document.getElementById('percentage3');
+let counter3 = 0;
+
+setInterval(() => {
+  if (counter3 == 87){
+    clearInterval;
+  }
+  else {
+    counter3 +=1;
+    number3.innerHTML = `${counter3}%`;
+  }
+}, 30);
+
+
+let number4 = document.getElementById('percentage4');
+let counter4 = 0;
+
+setInterval(() => {
+  if (counter4 == 85){
+    clearInterval;
+  }
+  else {
+    counter4 +=1;
+    number4.innerHTML = `${counter4}%`;
+  }
+}, 30);
+
+
